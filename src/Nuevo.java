ea459e5ec9d40c2b4c6333e77a1389f04b400ea7
JuanJuanitoJuan <juan.angel.ruiz.sanchez@gmail.com>
2021-08-13 17:28:18 -0500

    nuevo int numero1

--- src/Nuevo.java
+++ src/Nuevo.java
@@ -1,6 +1,7 @@
 public class Nuevo {
     public static void main(String[] args) {
+        int numero1 = 1
         int numero = 5;
-        System.out.println(numero);
+        System.out.println(numero); 
     }
 }
